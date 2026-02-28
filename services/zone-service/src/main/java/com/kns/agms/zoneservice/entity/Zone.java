3b631df01d260a7f19c6c209d3ddaa3604d86dd4
kameshNethsara <kamesh317nethsara@gmail.com>
2026-02-28 12:16:14 +0530

    add Zone entity and ZoneRepository for integration testing

---
+++ services/zone-service/src/main/java/com/kns/agms/zoneservice/entity/Zone.java
@@ -0,0 +1,25 @@
+package com.kns.agms.zoneservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "zones")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Zone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double minTemperature;
+    private Double maxTemperature;
+
+    private Double minHumidity;
+    private Double maxHumidity;
+}
