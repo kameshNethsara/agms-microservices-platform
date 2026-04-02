e391f7487009d87d6d6e60d3c9c10919f1e54be4
kameshNethsara <kamesh317nethsara@gmail.com>
2026-04-02 08:12:54 +0530

    feat: define SensorData Pydantic model for telemetry validation

--- services/sensor-service-fastapi/app/models/sensor_model.py
+++ services/sensor-service-fastapi/app/models/sensor_model.py
@@ -1,6 +1,9 @@
+# app/models/sensor_model.py
 from pydantic import BaseModel
+from typing import Optional, Dict
 
 class SensorData(BaseModel):
-    zoneId: int
-    temperature: float
-    humidity: float
+    deviceId: str
+    zoneId: str
+    value: Dict[str, float] # To capture {"temperature": 25.5, "humidity": 60}
+    capturedAt: Optional[str] = None
